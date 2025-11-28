e0a8f764cfc649fd7097ce9b9f7d7fd1b75d6572
assykot48 <assykot48@gmail.com>
2025-11-28 18:24:58 +0500

    amo data

---
+++ amo/config.php
@@ -0,0 +1,10 @@
+<?php
+$subdomain     = 'altayapart';
+$client_secret = ''; // Секретный ключ
+$client_id     = ''; // ID интеграции
+$code          = ''; // Код авторизации
+$redirect_uri  = 'http://m2property.ru/amo/amo.php';
+$domain = 'm2property.ru';
+$pipeline_id = 7885906;
+$user_amo = 10762602;
+$access_token = '';
